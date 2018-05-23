eb43076e19d41be01ccae5187300f2067dd86c54
He Zhang <rbczhang90@gmail.com>
2018-05-22 23:41:14 -0700

    Draw lines, circle, bg gradient, text

--- canvas.js
+++ canvas.js
@@ -12,8 +12,40 @@ function draw(startTime) {
 	var canvas = document.getElementById('canvas');
 	if (canvas.getContext) {
 		var ctx = canvas.getContext('2d');
-		ctx.clearRect(0,0,150,150);
+		
+		// Preserve current context state, such as fillStyle
+		ctx.save();
+
+		ctx.clearRect(0,0,300,300);
+
+		// Create a gradient background
+    var grd = ctx.createLinearGradient(0,0,200,0);
+		grd.addColorStop(0, "red");
+		grd.addColorStop(1, "white");
+		ctx.fillStyle = grd;
+		ctx.fillRect(0,0,300,300);
+
+    ctx.restore();
+
+    // Create a square moving down
 		ctx.fillRect(25,25+timeDiff,100,100);
+
+		// Create a cross
+		ctx.moveTo(0,0);
+		ctx.lineTo(300,300);
+		ctx.moveTo(0,300);
+		ctx.lineTo(300,0);
+		ctx.stroke();
+
+		// Create a half circle
+		ctx.beginPath();
+		ctx.arc(150,150,100,0,Math.PI);
+		ctx.stroke();
+
+		ctx.font = "30px Arial";
+		ctx.fillText("Henry", 100, 100);
+		ctx.strokeText("Rebecca", 200, 100);
+
 	}
 	window.requestAnimationFrame(function() {draw(startTime)});
 
