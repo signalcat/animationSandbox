5afc7dc306a0ab0ad025ca84ecac28743a86af6b
He Zhang <rbczhang90@gmail.com>
2018-05-27 21:26:22 -0700

    Add loops to update canvas

--- gamePlay.js
+++ gamePlay.js
@@ -1,7 +1,10 @@
 function startGame() {
     myGameArea.start();
     myGamePiece = new rectComponent(30, 30, "red", 10, 120);
+    
 }
+
+
 var myGameArea = {
     canvas: document.createElement("canvas"),
     start : function() {
@@ -9,6 +12,10 @@ var myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+    	this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
     }
 }
 
@@ -17,8 +24,17 @@ function rectComponent(width, height, color, x, y) {
 	this.height = height;
 	this.x = x;
 	this.y = y;
-	ctx = myGameArea.context;
-	ctx.fillStyle = color;
-	ctx.fillRect(this.x, this.y, this.width, this.height);
+	this.update = function() {
+		ctx = myGameArea.context;
+		ctx.fillStyle = color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);	
+	}
 }
+
+
+function updateGameArea() {
+	myGameArea.clear();
+	myGamePiece.update();	
+}
+
 
