39d6c3600ae3b0ccd1742a13f465a32ac529aee1
Henry Qin <root@hq6.me>
2018-07-16 23:39:26 -0700

    Make consistent with master

--- gamePlay.js
+++ gamePlay.js
@@ -1,15 +1,11 @@
 var myGamePiece;
+var myObstacles = [];
 
 function startGame() {
 
 
     myGamePiece = new rectComponent(30, 30, "red", 10, 120);
-
-    myUpBtn = new rectComponent(30,30,"blue",50,10);
-    myDownBtn = new rectComponent(30,30,"blue",50,70);
-    myLeftBtn = new rectComponent(30,30,"blue",20,40);
-    myRightBtn = new rectComponent(30,30,"blue",80,40);
-
+    myObstacle = new rectComponent(10, 200, "green", 300, 120);
     myGameArea.start();
 
 
@@ -46,6 +42,7 @@ canvas: document.createElement("canvas"),
             this.canvas.height = 270;
             // this.canvas.style.cursor = "none";
             this.context = this.canvas.getContext("2d");
+            this.frameNo = 0;
             document.body.insertBefore(this.canvas, document.body.childNodes[0]);
             this.interval = setInterval(updateGameArea, 20);
             window.addEventListener('mousedown', function(e){
@@ -67,8 +64,16 @@ canvas: document.createElement("canvas"),
                     myGameArea.y = false;
                     }) 
         },
-clear : function() {
-            this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+        clear : function() {
+                    this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+        },
+
+        stop : function() {
+            clearInterval(this.interval);
+        },
+
+        everyinterval : function(n) {
+            return ((this.frameNo / n) % 1 == 0);
         }
 }
 
@@ -77,10 +82,13 @@ function rectComponent(width, height, color, x, y) {
     this.height = height;
     this.x = x;
     this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+
     this.update = function() {
         ctx = myGameArea.context;
         ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);  
+        ctx.fillRect(this.x, this.y, this.width, this.height);	
     }
     this.clicked = function() {
         var myleft = this.x;
@@ -90,30 +98,83 @@ function rectComponent(width, height, color, x, y) {
         return ((myGameArea.y <= mybottom) && (myGameArea.y >= mytop)
                 && (myGameArea.x >= myleft) && (myGameArea.x <= myright));
     }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        return !((mybottom < othertop) ||
+               (mytop > otherbottom) ||
+               (myright < otherleft) ||
+               (myleft > otherright));
+    }
 }
+
 
 
 function updateGameArea() {
-    myGameArea.clear();
-    if (myGameArea.x && myGameArea.y) {
-        if (myUpBtn.clicked()) {
-            myGamePiece.y -= 1;
-        }
-        if (myDownBtn.clicked()) {
-            myGamePiece.y += 1;
-        }
-        if (myLeftBtn.clicked()) {
-            myGamePiece.x -= 1;
-        }
-        if (myRightBtn.clicked()) {
-            myGamePiece.x += 1;
+
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (myGamePiece.crashWith(myObstacles[i])) {
+            myGameArea.stop();
+            return;
         }
     }
-    myUpBtn.update();
-    myDownBtn.update();
-    myLeftBtn.update();
-    myRightBtn.update();
+    
+    myGameArea.clear();
+    myGameArea.frameNo += 1;
+    
+    // At the first frame or every 150(frame) * 20 ms (approx.),
+    // create a new obstacle
+    if (myGameArea.frameNo == 1 || myGameArea.everyinterval(150)) {
+        x = myGameArea.canvas.width;
+        y = myGameArea.canvas.height;
+        minHeight = 20;
+        maxHeight = 200;
+        height = Math.floor(Math.random()*(maxHeight - minHeight + 1) + minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random()*(maxGap - minGap + 1) + minGap);
+        myObstacles.push(new rectComponent(10, height, "green", x, 0));
+        myObstacles.push(new rectComponent(10, y - height - gap, "green", x, height + gap));
+    }
+
+    // Reduce unused previous obstacles
+    while (myObstacles.length > 0 && myObstacles[0].x < 0) {
+        myObstacles.shift();
+    }
+
+    for (i = 0; i < myObstacles.length; i += 1) {
+        myObstacles[i].x += -1;
+        myObstacles[i].update();
+    }
+    
+    myGamePiece.newPos();
     myGamePiece.update();
 }
 
+function moveup() {
+    myGamePiece.speedY -= 1;
+}
+function movedown() {
+    myGamePiece.speedY += 1;
+}
+function moveleft() {
+    myGamePiece.speedX -= 1;
+}
+function moveright() {
+    myGamePiece.speedX += 1;
+}
 
+function stopMove() {
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+}
